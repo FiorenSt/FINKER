0d305b63f116ab9e4926c36c96942b53a4fccc1f
Fiorenzo Stoppa <f.stoppa@astro.ru.nl>
2023-12-03 22:55:15 +0100

    v0.0.0

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -1,15 +1,25 @@
-# Example Script for Demonstrating Usage of Functions in utils.py
-import numpy as np
-import matplotlib.pyplot as plt
-from src.utils_SYNTHETIC_LIGHTCURVES import *
-from src.utils_FINKER import *
+from utils_SYNTHETIC_LIGHTCURVES import *
+from utils_FINKER import *
+from utils_Entropy import *
 
 
-# Generate a synthetic light curve
-t_observed, y_observed, uncertainties = SyntheticLightCurveGenerator.generate_synthetic_light_curve(
-    n_points=500, time=10, freq_primary=1, amplitude_primary=1, freq_secondary=1,
-    amplitude_secondary=0, eclipse_depth=0, baseline_magnitude=17.0,
-    noise_function=None, n_repeats=10, random_seed=42)
+# Create an instance of the class
+generator = SyntheticLightCurveGenerator()
+
+# Now call the method on this instance
+t_observed, y_observed, uncertainties = generator.generate_synthetic_light_curve(
+    n_points=500,
+    time=10,
+    freq_primary=1,
+    amplitude_primary=1,
+    freq_secondary=1,
+    amplitude_secondary=0,
+    eclipse_depth=0,
+    baseline_magnitude=17.0,
+    random_seed=None,
+    output_in_flux=True,
+    zero_point_flux=1.0
+)
 
 
 # Plotting the light curve
@@ -22,15 +32,16 @@ plt.gca().invert_yaxis()  # Inverting y-axis for magnitude
 plt.show()
 
 
+
 # Set up the frequency range for the search
-freq = np.linspace(0.001,2,1000)
+freq = np.linspace(0.001,5,10000)
 
 # Creating a FINKER instance
 finker = FINKER()
 
 # Running a parallel FINKER search
 best_freq, freq_err, result_dict = finker.parallel_nonparametric_kernel_regression(
-    t_observed=t_observed, 
+    t_observed=t_observed,
     y_observed=y_observed,
     uncertainties=uncertainties,
     freq_list=freq,
@@ -39,22 +50,17 @@ best_freq, freq_err, result_dict = finker.parallel_nonparametric_kernel_regressi
     regression_type='local_constant',
     bandwidth_method='custom',
     n_jobs=-2,
-    verbose=1,
-    search_width=0.001, #once the best frequency (or multiple potential ones) has been found, it will better search in a 0.1% range from that frequency
-    tight_check_points=1000,  #number of points used in the tight search
-    estimate_uncertainties=True, #enabling the uncertainties estimate procedure
-    n_bootstrap=1000, #number of repetitions of the procedure
-    bootstrap_width=0.005 #grid size of .5% around the best frequency to evaluate the uncertainties
-    bootstrap_points=100, #number of points used for the evaluation
+    tight_check_points=1000,
+    estimate_uncertainties=False
 )
 
 
 # Plot the results
 fig, ax = plt.subplots(figsize=(8, 6))
-ax.scatter(list(result_dict.keys()), list(result_dict.values()), label='Objective Value', s=1)
+ax.scatter(list(result_dict.keys()), list(result_dict.values()), s=1)
 ax.axvline(x=best_freq, color='g', linestyle='--', label=f'Best Frequency: {best_freq:.7f}', lw=2)
 ax.axvline(x=1, color='black', linestyle='--', label=f'True Frequency: {1:.7f}', lw=2)
 ax.set_xlabel('Frequency', fontsize=18)
 ax.set_ylabel('Squared Residuals', fontsize=18)
 ax.tick_params(axis='both', which='major', labelsize=14)
-ax.legend(loc='upper left', fontsize=14)
+ax.legend(loc='lower right', fontsize=14)
--- src/utils_SYNTHETIC_LIGHTCURVES.py
+++ src/utils_SYNTHETIC_LIGHTCURVES.py
@@ -3,102 +3,67 @@ import pandas as pd
 from astropy.table import Table
 import warnings
 from astropy.units import UnitsWarning
+from numpy.polynomial import Polynomial
 
 # Suppress UnitsWarning
 warnings.filterwarnings('ignore', category=UnitsWarning)
 
-
-
 class SyntheticLightCurveGenerator:
     def __init__(self):
-        pass
+        # Define the precomputed polynomial coefficients, domain, and window
+        self.precomputed_coefficients = [-0.00311296, 0.11068129, -0.69244663, 2.01098336, -2.56210283, 1.29726758]
+        self.domain = [13.79964161, 21.9997921]
+        self.window = [0, 1]
 
     @staticmethod
     def sort_on_x(x, y):
         zipp = list(zip(x, y))
         zipp.sort(key=lambda x: x[0])
         x, y = list(zip(*zipp))
-
         return np.array(x), np.array(y)
 
-
-    @staticmethod
-    def get_mag_stddev_relation(upper=12, lower=22, degree=5):
-
-        # Get magnitude standard deviation relation method
-        dat = Table.read('DATA/16010_ML1_16010_q_20210428_red_cat.fits', format='fits')
-        df = dat.to_pandas()
-        df = df.loc[((df.MAG_OPT > upper) & (df.MAG_OPT < lower))]
-        x = np.array(df.MAG_OPT.tolist())
-        y = np.sqrt(1.) * np.array(df.MAGERR_OPT.tolist())
-        xx, yy = SyntheticLightCurveGenerator.sort_on_x(x, y)
-        dy = pd.Series(yy)
-        yq = np.array(dy.rolling(500).quantile(0.25).tolist())
-
-        idx = np.isfinite(yq)
-        yq = yq[idx]
-        xx = xx[idx]
-
-        fx = np.polynomial.Polynomial.fit(xx, yq, degree, window=[0, 1])
-
-        ## !! returns function !!
-        return fx
+    def get_mag_stddev_relation(self):
+        # Returns a Polynomial object using the predefined coefficients
+        return Polynomial(self.precomputed_coefficients, domain=self.domain, window=self.window)
 
     @staticmethod
     def magnitude_to_flux(magnitude, zero_point_flux=1.0):
-        return zero_point_flux * np.power(10, -0.4 * (magnitude-23.9))
+        return zero_point_flux * np.power(10, -0.4 * (magnitude - 23.9))
 
     @staticmethod
     def calculate_flux_uncertainty(magnitude, magnitude_uncertainty, zero_point_flux=1.0):
         flux = SyntheticLightCurveGenerator.magnitude_to_flux(magnitude, zero_point_flux)
         return flux * np.log(10) * 0.4 * magnitude_uncertainty
 
-    @staticmethod
-    def generate_synthetic_light_curve(n_points=500, time=10, freq_primary=1,
+
+    def generate_synthetic_light_curve(self, n_points=500, time=10, freq_primary=1,
                                        amplitude_primary=1, freq_secondary=1,
                                        amplitude_secondary=0, eclipse_depth=0,
-                                       baseline_magnitude=17.0,
-                                       random_seed=None, output_in_flux=True, zero_point_flux=1.0):
+                                       baseline_magnitude=17.0, random_seed=None,
+                                       output_in_flux=True, zero_point_flux=1.0):
         if random_seed is not None:
             np.random.seed(random_seed)
 
-        noise_function = SyntheticLightCurveGenerator.get_mag_stddev_relation()
+        noise_function = self.get_mag_stddev_relation()
 
         # Time vector
         t_observed = np.sort(np.random.uniform(0, time, n_points))
-        # Generate primary frequency component
-        y_primary = - amplitude_primary * np.sin(2 * np.pi * freq_primary * t_observed)
 
-        # Generate secondary frequency component
+        # Generate light curve components
+        y_primary = - amplitude_primary * np.sin(2 * np.pi * freq_primary * t_observed)
         y_secondary = - amplitude_secondary * np.sin(2 * np.pi * freq_secondary * t_observed)
-        # Generate eclipse (simplified)
         y_eclipse = eclipse_depth * np.sin(2 * np.pi * freq_secondary * t_observed) ** 30
-        # Combine to get the complex light curve in magnitudes
         y_magnitude = baseline_magnitude + y_primary + y_secondary + y_eclipse
 
-
+        # Apply noise
         noise_std = noise_function(y_magnitude)
-
         uncertainties = np.random.normal(0, noise_std, n_points)
         y_magnitude_observed = y_magnitude + uncertainties
 
-        # Calculate the mean and standard deviation of the repeated observations
-        sigma = noise_std
-
+        # Output handling
         if output_in_flux:
-            # Convert magnitudes to flux
             y_flux_observed = SyntheticLightCurveGenerator.magnitude_to_flux(y_magnitude_observed, zero_point_flux)
-
-            # Calculate flux uncertainties
-            flux_uncertainties = SyntheticLightCurveGenerator.calculate_flux_uncertainty(y_magnitude_observed, sigma,
-                                                                                         zero_point_flux)
-
+            flux_uncertainties = SyntheticLightCurveGenerator.calculate_flux_uncertainty(y_magnitude_observed, noise_std, zero_point_flux)
             return t_observed, y_flux_observed, np.abs(flux_uncertainties)
         else:
-            # Return in magnitude
-            return t_observed, y_magnitude_observed, sigma
-
-
-
-
-
+            return t_observed, y_magnitude_observed, noise_std
